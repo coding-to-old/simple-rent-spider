601121a63ddb7c36256b5c6fc354c3f1f29f5837
ybbzbb <youcamhhhh@qq.com>
2019-09-22 21:15:33 +0800

    init spider

---
+++ src/main/java/cto/github/rent/RentSpiderApplication.java
@@ -0,0 +1,13 @@
+package cto.github.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentSpiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentSpiderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cto/github/rent/RentSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package cto.github.rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSpiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
