47d8890c686823fcddaadaf058b99e90e255da93
ybbzbb <youcamhhhh@qq.com>
2019-09-24 23:20:37 +0800

    项目初始化

---
+++ src/main/java/cto/github/rent/task/DoubanSpiderTask.java
@@ -0,0 +1,14 @@
+package cto.github.rent.task;
+
+/**
+ * @Date 2019-09-24
+ * @author ybbzbb
+ * 豆瓣爬虫启动类
+ * */
+public class DoubanSpiderTask implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
