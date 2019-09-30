34caec7d3147019567c5531737d6a0442916aa4f
ruirui.zheng@esrtech.cn <ruirui.zheng@esrtech.cn>
2019-09-30 15:38:52 +0800

    项目结构化

--- src/main/java/cto/github/rent/RentSpiderApplication.java
+++ src/main/java/cto/github/rent/RentSpiderApplication.java
@@ -2,8 +2,10 @@ package cto.github.rent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import simple.reat.bast.UtilPlugin;
 
 @SpringBootApplication
+@org.springframework.context.annotation.Import({UtilPlugin.class})
 public class RentSpiderApplication {
 
 	public static void main(String[] args) {
